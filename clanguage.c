4c80e8c89a2af5adb455172c7ca0c863cc2a80b7
Harshsanjaybhaipatel <144709251+Harshsanjaybhaipatel@users.noreply.github.com>
2023-09-11 21:15:32 +0530

    Update clanguage.c

--- clanguage.c
+++ clanguage.c
@@ -1,1 +1,17 @@
+#include<stdio.h>
+  int main()
+{
+  int number;
 
+    printf("Enter the number\n");
+  scanf("%d",&number);
+
+  if (number>0){
+    printf("The number is positive");
+  }
+  else if (number < 0) {
+        printf("The number is negative.\n");
+    } else {
+        printf("The number is zero.\n");
+    }
+}
