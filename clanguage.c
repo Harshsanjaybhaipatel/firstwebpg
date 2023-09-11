a7866290b6d289aca81cca23bd77f63941144533
Harshsanjaybhaipatel <144709251+Harshsanjaybhaipatel@users.noreply.github.com>
2023-09-11 20:57:42 +0530

    Update clanguage.c

--- clanguage.c
+++ clanguage.c
@@ -1,1 +1,13 @@
-
+#include<stdio.h>
+    int main()
+    {
+        int a = 40;
+      int b=30;
+      int c=10;
+      if(a>b){
+        printf("a is greater than b\n");
+        }
+      else if(b>c){
+        printf("b is greater than c");
+    }
+    }
